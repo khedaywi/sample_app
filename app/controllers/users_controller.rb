6a6d1d9bd13afbf4ec50b12ffc1bfcb083e1a814
khedaywi <khedaywi@gmail.com>
2013-06-21 16:43:36 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def sign_up
+  def show
+    @user = User.find(params[:id])
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)    
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      flash[:error] = "You fucked up big time!"
+      render 'new'
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                     :password_confirmation)
+    end
+ 
 end
